54e64e6e1018e488c1f7aef8d508bf0ea50d679b
Lluis Oliver <oliverobrador@gmail.com>
2014-07-16 15:58:36 +0100

    Some improvements

--- springy/js/springy.custom.js
+++ springy/js/springy.custom.js
@@ -2,6 +2,8 @@
     $(document).ready(function() {
         iac();
         filesNewTab();
+        prettyButtons();
+        prettyForms();
     });
 
     function iac() {
@@ -15,6 +17,16 @@
 
     function filesNewTab() {
         $('.field-type-file').find('a').attr('target', '_blank');
+    }
+
+    function prettyButtons() {
+        $('input[type=submit]').addClass('pretty medium default btn');
+    }
+
+    function prettyForms() {
+        $('input[type=text], input[type=email], input[type=password]').addClass('input').wrap('<div class="field"></div>');
+        $('textarea').addClass('input textarea').wrap('<div class="field"></div>');
+        $('select').wrap('<div class="field"><div class="picker"></div></div>');
     }
 
 })(jQuery);
