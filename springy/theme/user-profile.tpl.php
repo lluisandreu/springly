3bc9f94c58f6a65ac3b9195c998dc3b5d8e69a60
Lluis Oliver <oliverobrador@gmail.com>
2014-09-17 15:29:43 +0100

    Fixed bug in User Profile

--- springy/theme/user-profile.tpl.php
+++ springy/theme/user-profile.tpl.php
@@ -5,7 +5,7 @@
     <?php
     $userid = arg(1);
     global $user;
-    if($user->uid && is_numeric($userid)) {
+    if($user->uid == is_numeric($userid) || $user->uid == 1) {
             print '<div class="iac iac-edit pretty medium primary btn icon-left icon-vcard"><a href="'.url('user/'.$userid.'/edit').'">Edit profile</a></div>';
     }
 ?>
