75275ca22305c863ce40e1e603b6767f29b8c8d4
Lluis Oliver <oliverobrador@gmail.com>
2014-10-10 16:02:09 +0100

    Small changes in js

--- springy/js/springy.custom.js
+++ springy/js/springy.custom.js
@@ -1,31 +1,34 @@
-iac();
-filesNewTab();
+(function() {
 
-function iac() {
+    iac();
+    filesNewTab();
 
-    var container = document.querySelectorAll('.iac-container');
-    var buttons = document.querySelectorAll('.iac');
+    function iac() {
 
-    for (var i = buttons.length - 1; i >= 0; i--) {
-        buttons[i].style.display = "none";
-    };
+        var container = document.querySelectorAll('.iac-container');
+        var buttons = document.querySelectorAll('.iac');
 
-    for (var i = container.length - 1; i >= 0; i--) {
-        container[i].onmouseover = function() {
-            var iac = this.querySelectorAll('.iac');
-            iac[0].style.display = "block";
-        }
-        container[i].onmouseout = function() {
-            var iac = this.querySelectorAll('.iac');
-            iac[0].style.display = "none";
-        }
-    };
-}
+        for (var i = buttons.length - 1; i >= 0; i--) {
+            buttons[i].style.display = "none";
+        };
 
-function filesNewTab() {
-    var link = document.querySelectorAll('.field-type-file');
-    for (var i = link.length - 1; i >= 0; i--) {
-        var a = link[i].getElementsByTagName('a');
-        a[0].setAttribute('target', '_blank');
-    };
-}
+        for (var i = container.length - 1; i >= 0; i--) {
+            container[i].onmouseover = function() {
+                this.querySelector('.iac').style.display = "block";
+            }
+            container[i].onmouseout = function() {
+                this.querySelector('.iac').style.display = "none";
+            }
+        };
+
+    }
+
+    function filesNewTab() {
+        var link = document.querySelectorAll('.field-type-file');
+        for (var i = link.length - 1; i >= 0; i--) {
+            var a = link[i].getElementsByTagName('a');
+            a[0].setAttribute('target', '_blank');
+        };
+    }
+
+})();
