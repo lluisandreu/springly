9a0d34910984128cf4832516e15a2154c1351b51
Lluis Oliver <oliverobrador@gmail.com>
2014-10-20 15:17:41 +0100

    Fixing

--- springy/js/springy.custom.js
+++ springy/js/springy.custom.js
@@ -2,25 +2,27 @@
 
     iac();
     filesNewTab();
+    svgFallback();
 
     function iac() {
 
         var container = document.querySelectorAll('.iac-container');
         var buttons = document.querySelectorAll('.iac');
 
-        for (var i = buttons.length - 1; i >= 0; i--) {
-            buttons[i].style.display = "none";
-        };
+        if (buttons.length != 0) {
+            for (var i = buttons.length - 1; i >= 0; i--) {
+                buttons[i].style.display = "none";
+            };
 
-        for (var i = container.length - 1; i >= 0; i--) {
-            container[i].onmouseover = function() {
-                this.querySelector('.iac').style.display = "block";
-            }
-            container[i].onmouseout = function() {
-                this.querySelector('.iac').style.display = "none";
-            }
-        };
-
+            for (var i = container.length - 1; i >= 0; i--) {
+                container[i].onmouseover = function() {
+                    this.querySelector('.iac').style.display = "block";
+                }
+                container[i].onmouseout = function() {
+                    this.querySelector('.iac').style.display = "none";
+                }
+            };
+        }
     }
 
     function filesNewTab() {
@@ -29,6 +31,19 @@
             var a = link[i].getElementsByTagName('a');
             a[0].setAttribute('target', '_blank');
         };
+    }
+
+    function svgFallback() {
+        if (!Modernizr.svg) {
+            var imgs = document.getElementsByTagName('img');
+            var svgExtension = /.*\.svg$/
+            var l = imgs.length;
+            for (var i = 0; i < l; i++) {
+                if (imgs[i].src.match(svgExtension)) {
+                    imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+                }
+            }
+        }
     }
 
 })();
--- springy/theme/blocks/block.tpl.php
+++ springy/theme/blocks/block.tpl.php
@@ -1,11 +1,11 @@
-<div id="<?php print $block_html_id; ?>" class="block">
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
 <?php if ($block->subject): ?>
   <h2 class="block__title"><?php print $block->subject ?></h2>
 <?php endif;?>
   <?php print render($title_suffix); ?>
-  <div class="block__content">
+  <div class="block__content"<?php print $content_attributes; ?>>
     <?php print $content ?>
   </div>
 </div>
